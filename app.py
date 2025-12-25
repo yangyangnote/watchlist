6dc8b7d4cbd12509462462ed831a82638ff66a8c
Arya yang <yangyang.swift@gmail.com>
2025-12-25 10:08:51 +0800

    Add the index page template

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, url_for
 from markupsafe import escape
+
+name = "Grey Li"
+movies = [
+    {"title": "My Neighbor Totoro", "year": "1988"},
+    {"title": "Dead Poets Society", "year": "1989"},
+    {"title": "A Perfect World", "year": "1993"},
+    {"title": "Leon", "year": "1994"},
+    {"title": "Mahjong", "year": "1996"},
+    {"title": "Swallowtail Butterfly", "year": "1996"},
+    {"title": "King of Comedy", "year": "1999"},
+    {"title": "Devils on the Doorstep", "year": "1999"},
+    {"title": "WALL-E", "year": "2008"},
+    {"title": "The Pork of Music", "year": "2012"},
+]
 
 # 这句是从 flask 包里导入 Flask 类
 app = Flask(__name__)
@@ -9,10 +23,8 @@ app = Flask(__name__)
 
 # 当浏览器访问根路径 /时，调用下面这个函数
 @app.route("/")
-@app.route("/index")
-@app.route("/home")
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template("index.html", name=name, movies=movies)
 
 
 @app.route("/user/<name>")
@@ -23,7 +35,7 @@ def user_page(name):
 @app.route("/test")
 def test_url_for():
     # 下面是一些调用示例（访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for("hello"))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    print(url_for("index"))  # 生成 index 视图函数对应的 URL，将会输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for("user_page", name="greyli"))  # 输出：/user/greyli
     print(url_for("user_page", name="peter"))  # 输出：/user/peter
